a5f74f2b1721b65e7f20c172ace80bbac6de499d
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-14 09:38:48 +0330

    :boom:

--- kelassor/battle/admin.py
+++ kelassor/battle/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User, Message
+from django.utils import timezone
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "phone_number")
+    list_filter = ("firstname", "phone_number")
+    list_per_page = 20
+    
+
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return ("firstname", "lastname")
+        return ("firstname", )
+    
+
+
+admin.site.register(Message)
+admin.site.register(User)
+
+
